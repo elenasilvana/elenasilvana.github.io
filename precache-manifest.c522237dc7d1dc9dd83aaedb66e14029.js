c2f30c14d1c4c804ea4e0cf30fea39ee8faccfdf
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-06-15 20:07:30 -0500

    Updates

---
+++ precache-manifest.c522237dc7d1dc9dd83aaedb66e14029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d056e324f1db876e611b4ed947335d26",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "df0fad65b8e2d8a7a24b",
+    "url": "/portafolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portafolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "df0fad65b8e2d8a7a24b",
+    "url": "/portafolio/static/js/main.49131a2a.chunk.js"
+  },
+  {
+    "revision": "9fd29bcc1d4ba73bed23",
+    "url": "/portafolio/static/js/runtime~main.41300385.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portafolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
