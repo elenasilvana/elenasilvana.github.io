37306940fe2cabbe23fc2141d00240dbd1d7b72a
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-06-16 17:41:02 -0500

    some text added to about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const About = () => (
     <div>
-        <h1>About me</h1>
+        <h1>Hi, are you looking for me?</h1>
+        <p>I’m Elena, I’m a front end developer
+do you liked my work? get in touch</p>
     </div>
 
 );
