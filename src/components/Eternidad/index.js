8fee91d7b2f014ae3768be9933be53265a0837e3
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-06-16 14:06:39 -0500

    arts projects component files created

---
+++ src/components/Eternidad/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Eternidad = () => (
+    <div>
+        <h1>Soy Eternidad, un proyecto de fotografía</h1>
+    </div>
+
+);
+
+export default Eternidad;
---
+++ src/components/ObraGris/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ObraGris = () => (
+    <div>
+        <h1>Soy Obra Gris, un proyecto de fotografía</h1>
+    </div>
+
+);
+
+export default ObraGris;
