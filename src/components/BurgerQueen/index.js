d1266fa2c0c88aa22916fc7689bedcafaab2f131
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-06-16 14:05:46 -0500

    About, Home and front-end projects component files created

---
+++ src/components/BurgerQueen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BurgerQueen = () => (
+    <div>
+        <h1>Soy el componente de BurgerQueen</h1>
+    </div>
+
+);
+
+export default BurgerQueen;
---
+++ src/components/Cipher/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Cipher = () => (
+    <div>
+        <h1>Soy Cipher</h1>
+    </div>
+
+);
+
+export default Cipher;
