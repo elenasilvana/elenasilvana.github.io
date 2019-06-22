54f326e5b1ab1ff991efd376befca7abb07d3dd0
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-06-22 18:21:32 -0500

    working on render items structure

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,7 +6,9 @@ const projects = [
     {
         name: 'Conoce Pokemón',
         img: 'https://i.postimg.cc/mZnqwf58/alola-form-kanto-pokemon-go.jpg',
-        category: 'front-end'
+        category: 'front-end',
+        deliverable: 'WebApplication'
+
     }
 ];
 
@@ -18,22 +20,33 @@ const Home = () => (
     <container className="portfolio-container">
 
         <div className="portfolio-home">
-            <h1>Elena Silvana</h1>
-            <Link className="btn btn-outline-secondary btn-lg"  to={ROUTES.ABOUT}>About</Link>
+            <h1 id="ES">Elena Silvana</h1>
+            <p>hola mundo</p>
+            <Link className="btn btn-light btn-lg about-botton"  to={ROUTES.ABOUT}>About</Link>
         </div>
 
-        <div className="navigation-widget"></div>
+        <div className="navigation-widget">
+            <p><a href="#ES"></a>Home</p>
+            {projects.map((project, key)=> (
+                <p><a href={`#${project.name}`}></a>{project.name}</p>
+
+                
+            ))}
+        </div>
        
 
         <div className="project-list">
             <p></p>
             
-            {/*projects.map((project, index)=> (
-                <div>
-                    <p>{project.name}</p>
+            {projects.map((project, key)=> (
+                <div className="project-card">
+                    <div className="project-img" style={{backgroundImage: "url(" + project.img + ")"}}>
+                    <p id="project.name">{project.name}</p>
+                    <p>{project.deliverable}</p>
+                    </div>
                 </div>
 
-            ))*/}
+            ))}
         </div>
        
 
