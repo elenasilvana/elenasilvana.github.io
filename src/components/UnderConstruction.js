0480a6f4fad8187414bfb95c37aa1f001294ad6c
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-07-04 12:10:44 -0500

    component message for pages under construction

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const UnderConstruction = () => (
+                        
+    <div className="row">
+        <div className="col">
+                <div className="name">
+                    <p className="text-center">Sorry, I'm working on the content of this page</p>
+            </div>
+        </div>
+    </div>
+
+
+);
+
+export default UnderConstruction;
+
+                    
