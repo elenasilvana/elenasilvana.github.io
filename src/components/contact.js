bd591e73078fc815b5efa41dd0e1b06d55fccad1
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-07-24 22:19:42 -0500

    working on contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
-class Contact extends Component{
-    render(){
-        return(
-            <div><h1>Contact Page</h1></div>
+class Contact extends Component {
+    render() {
+        return (
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Elena Silvana</h2>
+                        <img
+                            src="https://avatarfiles.alphacoders.com/125/125495.jpg"
+                            alt="avatar"
+                            style={{ height: '250px' }}></img>
+                        <p style={{ margin: 'auto,', paddingTop: '1em' }}>
+                            Front end developer formada en Laboratoria, egresada de la licenciatura en artes visuales de la secretaría de cultura Jalisco
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Merriweather Sans'}}>
+                                        <i className="fa fa-phone-square" aria-hidden='true'/>
+                                        (33) 36 26 45 79
+                                        </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Merriweather Sans'}}>
+                                        <i className="fa fa-envelope" aria-hidden='true'/>
+                                        ele.silvana.c@gmail.com
+                                        </ListItemContent>
+                                </ListItem><ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Merriweather Sans'}}>
+                                        <i className="fa fa-phone-square" aria-hidden='true'/>
+                                        linkedin
+                                        </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+
+            </div>
         )
     }
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,122 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
-class Projects extends Component{
-    render(){
-        return(
-            <div><h1>Projects Page</h1></div>
+class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeTab: 0
+        };
+    }
+
+    toggleCategories = () => {
+        if (this.state.activeTab === 0) {
+            /*Pokemon project Javascript */
+            return (
+                <div  className="projects-grid">      
+                <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                    <CardTitle
+                        style={{
+                            color: '#fff',
+                            height: '176px',
+                            background: 'url(https://i.postimg.cc/LXxCwrvQ/pokeball.png) center / cover'
+                        }}
+                    >
+                        Javascript Project
+                    </CardTitle>
+                    <CardText>
+                        Conoce Pokemón es una WebApplication para conocer pokemón
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>LiveDemo</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{ color: '#fff' }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                </div>
+            )
+        }
+        else if (this.state.activeTab === 1) {
+            /* React Project */
+            return (
+                <div  className="projects-grid">
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle
+                            style={{
+                                color: '#fff',
+                                height: '176px',
+                                background: 'url(https://i.postimg.cc/LXxCwrvQ/pokeball.png) center / cover'
+                            }}
+                        >
+                            React JS
+                    </CardTitle>
+                        <CardText>
+                            Burger queen
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>Github</Button>
+                            <Button colored>LiveDemo</Button>
+                            <Button colored>Github</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            )
+        }
+        else if (this.state.activeTab === 2) {
+            /* Node JS project */
+            return (
+                <div  className="projects-grid">             
+                <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                    <CardTitle
+                        style={{
+                            color: 'black',
+                            height: '176px',
+                            background: 'url(https://i.postimg.cc/LXxCwrvQ/pokeball.png) center / cover'
+                        }}
+                    >
+                        Node JS
+                </CardTitle>
+                    <CardText>
+                        API REST BQ
+                </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>LiveDemo</Button>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{ color: '#fff' }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                </div>
+            )
+        }
+
+    }
+
+
+    render() {
+        return (
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>Javascript</Tab>
+                    <Tab>React</Tab>
+                    <Tab>NodeJS</Tab>
+                </Tabs>
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}
+
+                            </div>
+                        </Cell>
+                    </Grid>
+            </div>
         )
     }
 }
