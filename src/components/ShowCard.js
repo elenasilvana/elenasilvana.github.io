a7867270cb23246f35032c22420f05b07033e741
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2019-07-26 12:12:29 -0500

    component showCard created

---
+++ src/components/ShowCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+
+
+class ShowCard extends Component {
+    render(){
+        return(
+
+        )
+    }
+} 
+
+export default ShowCard;
