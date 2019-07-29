dadf969a48fdc8ce8343db77e4c3bc0b064d2bea
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2019-07-28 20:42:55 -0500

    links added to icons

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -2,31 +2,35 @@ import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
 
-class Landing extends Component{
-    render(){
-        return(
-            <div style={{width: '100%', margin: 'auto'}}>
+class Landing extends Component {
+    render() {
+        return (
+            <div style={{ width: '100%', margin: 'auto' }}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                    <img
-                    src="https://i.postimg.cc/Y98PQvcg/Whats-App-Image-2019-07-28-at-19-46-16.jpg"
-                    alt="avatar"
-                    className="avatar-img">       
-                    </img>
-                    <div className="banner-text">
-                        <h2>Front End Developer</h2>
-                        <hr/>
-                        <p> HTML/CSS | Bootstrap | Javascript | React | NodeJS | Express | MongoDB</p>
-                        <div className="social-links">
-                            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-github" aria-hidden="true"/>
-                            </a>
-                            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-linkedin-square" aria-hidden="true"/>
-                            </a>
+                        <img
+                            src="https://i.postimg.cc/Y98PQvcg/Whats-App-Image-2019-07-28-at-19-46-16.jpg"
+                            alt="avatar"
+                            className="avatar-img">
+                        </img>
+                        <div className="banner-text">
+                            <h2>Front End Developer</h2>
+                            <hr />
+                            <p> HTML/CSS | Bootstrap | Javascript | React | NodeJS | Express | MongoDB</p>
+                            <div className="social-links">
+                                <a
+                                    target="_blank"
+                                    href="https://github.com/elenasilvana">
+                                    <i className="fa fa-github" aria-hidden='true' />
+                                </a>
+                                <a
+                                    target="_blank"
+                                    href="https://www.linkedin.com/in/elena-silvana-c/">
+                                    <i className="fa fa-linkedin" aria-hidden='true' />
+                                </a>
 
+                            </div>
                         </div>
-                    </div>
                     </Cell>
                 </Grid>
             </div>
