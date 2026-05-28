26867be661d9dcb7bc0cbaec2da6c1a36b6f7710
Elena Silvana Casillas <stenocereus.queretaroensis@gmail.com>
2026-05-28 11:51:44 +0200

    experienceListItem component created

---
+++ app/components/experienceListItem.tsx
@@ -0,0 +1,51 @@
+import { ListItem, ListItemText, Typography } from "@mui/material";
+import React from "react";
+
+export const ExperienceListItem = ({
+    title,
+    institution,
+    dateRange,
+    location,
+    hasSingleRow,
+    hasSlashDivider
+}: any) => {
+    const { startDate, endDate } = dateRange;
+    const dateDivider = hasSlashDivider ? '/' : '-';
+    const date = `${startDate} ${dateDivider} ${endDate}`
+    const dot = '·'
+    const titleFormat = hasSingleRow ? `${title} ${dot} ${institution}` : title
+
+    return (
+        <ListItem>
+            <ListItemText
+                primary={
+                    <Typography variant="h4" color="secondary">
+                        {date}
+                    </Typography>
+                }
+                secondary={
+                    <React.Fragment>
+                        <Typography
+                            component="span"
+                            variant="body1"
+                            sx={{ color: 'secondary', display: 'inline' }}
+                        >
+                            {titleFormat}
+                        </Typography>
+                        {!hasSingleRow && (
+                            <Typography color="primary">
+                                {institution}
+                            </Typography>
+                        )}
+                        {location && (
+                            <Typography>
+                                {location}
+                            </Typography>
+                        )}
+                    </React.Fragment>
+                }
+            />
+        </ListItem>
+    )
+
+}
