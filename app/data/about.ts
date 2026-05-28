47ef5fd017ff592294e9e4f94312256aa316390c
Elena Silvana Casillas <stenocereus.queretaroensis@gmail.com>
2026-05-28 11:52:44 +0200

    about section updated to use ExperienceListItem and refactored

---
+++ app/data/about.ts
@@ -0,0 +1,21 @@
+
+export const educationDetails = [
+    {
+        "title": "Bachelor degree on Visual Arts",
+        "institution": "Escuela de Artes de la Secretaría de Cultura Jalisco",
+        "dateRange": { "startDate": 2012, "endDate": 2017 },
+        "location": "",
+    },
+    {
+        "title": "Front-End Developer",
+        "institution": "Laboratoria",
+        "dateRange": { "startDate": 2019, "endDate": 2019 },
+        "location": "",
+    }
+]
+
+export const languageDetails = [
+    { "language": "spanish", "level": 'native' },
+    { "language": "english", "level": 'fluent' },
+    { "language": "german", "level": 'intermediate' }
+]
